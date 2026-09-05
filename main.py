66e5a7b78e0c91ac5b6076bb587342dc3cf667f5
Miguel <mmourasoeiro17@gmail.com>
2026-09-05 11:11:02 +0100

    add validation that max duration is at least min duration

--- main.py
+++ main.py
@@ -16,11 +16,14 @@ def ask_for_date(question):
         except ValueError:
             print("Invalid date, try again")
 
-max_budget = ask_for_int("What is the maximum budget? ")
-beggining_date = ask_for_date("What is the beggining date? ")
+max_budget = ask_for_int("What is the maximum budget? (euros)")
+beginning_date = ask_for_date("What is the beginning date? ")
 end_date = ask_for_date("What is the end date? ")
-while end_date < beggining_date:
+while end_date < beginning_date:
     print("End date must be after beginning date, try again")
     end_date = ask_for_date("What is the end date? ")
-min_duration = ask_for_int("What is the minimum duration? ")
-max_duration = ask_for_int("What is the maximum duration? ")
+min_duration = ask_for_int("What is the minimum duration? (days) ")
+max_duration = ask_for_int("What is the maximum duration? (days) ")
+while max_duration < min_duration:
+    print("Minimum duration should be lower than maximum duration, try again")
+    max_duration = ask_for_int("What is the maximum duration? (days) ")
