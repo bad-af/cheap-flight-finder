b33720dd5b77059a03a6e78b2d22e6f4c1cdabc8
Miguel <mmourasoeiro17@gmail.com>
2026-09-05 10:53:44 +0100

    add date validation with ask_for_date function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def ask_for_int(question):
     while True:
         try:
@@ -6,9 +8,17 @@ def ask_for_int(question):
         except ValueError:
             print("Invalid input, try again")
 
+def ask_for_date(question):
+    while True:
+        try:
+            answer = datetime.strptime( input(question), "%d/%m/%Y")
+            return answer
+        except ValueError:
+            print("Invalid date, try again")
+
 max_budget = ask_for_int("What is the maximum budget? ")
-beggining_date = input("Beginning date? ")
-end_date = input("End date? ")
+beggining_date = ask_for_date("What is the beggining date? ")
+end_date = ask_for_date("What is the end date? ")
 min_duration = ask_for_int("What is the minimum duration? ")
 max_duration = ask_for_int("What is the maximum duration? ")
-print(max_budget, beggining_date, end_date, min_duration, max_duration)
+print(end_date - beggining_date)
